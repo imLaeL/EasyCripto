97974887b9de8a36d581f25e63b95070d48e130c
Delieudo <joaoaguiar.fn@gmail.com>
2024-05-14 08:45:04 -0300

    Hash256

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 #Criptografia Bcrypt
-
+import hashlib
 import bcrypt
+
 password = input('Digite a senha que deseja criptografar: ')
 bytes = password.encode('utf-8')
 salt = bcrypt.gensalt()
 hash = bcrypt.hashpw(bytes, salt)
 
 print(f'Essa é a sua nova senha criptografada: {hash}')
+
+#Criptografia 256
+
+texto = "Aprendendo a criar hash com SHA256"
+hash = hashlib.sha256(texto.encode()).hexdigest()
+
+print("Texto original:", texto)
+print("Hash SHA256:", hash)
