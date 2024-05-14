659b75ad126d8ea2e39d864c53309c11ab5cb44a
João <joaoestevam.1204@gmail.com>
2024-05-13 21:53:53 -0300

    Bcryto adição

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+#Criptografia Bcrypt
 
+import bcrypt
+password = input('Digite a senha que deseja criptografar: ')
+bytes = password.encode('utf-8')
+salt = bcrypt.gensalt()
+hash = bcrypt.hashpw(bytes, salt)
+
+print(f'Essa é a sua nova senha criptografada: {hash}')
